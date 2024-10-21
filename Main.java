a7266727434115114584fc34c3aea2b8d1744bee
leannec8 <149003503+leannec8@users.noreply.github.com>
2024-10-21 13:32:56 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        WordChecker w = new WordChecker();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        System.out.println(words);
+        WordChecker x = new WordChecker(words);
+    }
+}
+
+//page 3 middle
